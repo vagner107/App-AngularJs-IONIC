5f5e07f06159e690237e386cc5d310648297e9ed
VAGNER <vagnerarau@gmail.com>
2015-07-19 16:09:07 -0300

    teste

--- www/js/app.js
+++ www/js/app.js
@@ -9,6 +9,14 @@ angular.module('starter', ['ionic', 'ionic.service.core', 'ionic.service.deploy'
 .run(function($ionicPlatform) {
 
   $ionicPlatform.ready(function() {
+	  
+	setTimeout(function() {
+        navigator.splashscreen.hide();
+    }, 100);  
+	
+	 if(window.navigator && window.navigator.splashscreen) {
+      window.navigator.splashscreen.hide();
+    }
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
